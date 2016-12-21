f89acdcaa7903a957295b364abbe0a5f920ab0b9
fafa <fafa2088@live.com>
2016-12-21 10:24:17 +0800

    clearn

--- test/__cache/menu.cache.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('IS_RUN') or exit('/**error:404**/');
-return array (
-  0 => 'home',
-  1 => 'book',
-);
-?>
--- test/__init.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('IS_RUN') or exit('/**error:404**/');
-/**
-*公共配置
-*/
-echo 'loading : '.__FILE__.'<br />';
-?>
